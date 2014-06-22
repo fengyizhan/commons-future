1b9bbf856c1b191ab20887e8ec8525d6003b9c49
qiyi <bphanzhu@gmail.com>
2014-06-22 21:45:30 +0800

    add comment

--- src/main/java/org/isouth/commons/future/TaskCallback.java
+++ src/main/java/org/isouth/commons/future/TaskCallback.java
@@ -2,6 +2,9 @@ package org.isouth.commons.future;
 
 public abstract class TaskCallback extends Functional<TaskFuture, TaskFuture> {
 
+    /**
+     * 执行动作
+     */
     @Override
     public abstract TaskFuture apply(TaskFuture f);
 }
